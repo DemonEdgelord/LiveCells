3ddb39ac258d520589bef10bd38f1c647518512a
DemonEdgelord <42448306+DemonEdgelord@users.noreply.github.com>
2018-08-21 20:45:08 +0500

    WIP

--- LiveCells/Assets/Scripts/Enemies/Golem/Movement_Controller.cs
+++ LiveCells/Assets/Scripts/Enemies/Golem/Movement_Controller.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Movement_Controller : MonoBehaviour {
+public class Movement_Controller : MonoBehaviour
+{
 
     public Transform playerLocation;
+
+    private void Update()
+    {
+        FindPlayer();
+    }
+
+    private RaycastHit2D rayHit;
+    public Transform rayOrigin;
+
+    void FindPlayer()
+    {
+        Physics2D.Raycast(rayOrigin.position, rayOrigin.forward, rayHit, 5);
+        Debug.DrawRay();
+    }
 
 }
