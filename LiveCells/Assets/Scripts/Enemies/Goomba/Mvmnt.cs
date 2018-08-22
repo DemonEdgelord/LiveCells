8a55bfdabfcb59ff7b40da126e943a8e2f1de320
DemonEdgelord <42448306+DemonEdgelord@users.noreply.github.com>
2018-08-22 16:36:39 +0500

    WIP

---
+++ LiveCells/Assets/Scripts/Enemies/Goomba/Mvmnt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Mvmnt : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public float speed;
+    public float direction = 1;
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(5 * direction, rb.velocity.y);
+    }
+    public void Turn()
+    {
+        direction = direction * -1;
+    }
+
+}
---
+++ LiveCells/Assets/Scripts/Enemies/Goomba/TurningPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TurningPoint : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Turningpoint"))
+        {
+            col.gameObject.GetComponent<Mvmnt>().Turn();
+            Debug.Log("Colision detected");
+        }
+    }
+
+}
