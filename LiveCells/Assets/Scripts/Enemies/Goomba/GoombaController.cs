85b0a7fcc9ee29d46e9ef44d075a44f0e36ffe24
Gazarius <Talhakaleem4411@gmail.com>
2018-08-22 14:52:30 +0500

    the goomba

---
+++ LiveCells/Assets/Scripts/Enemies/Goomba/GoombaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    void starrt()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public float speed;
+    void Update()
+    {
+        rb.velocity = new Vector2(5, 0);
+    }
+}
